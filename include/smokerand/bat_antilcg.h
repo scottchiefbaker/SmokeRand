c471441fb3c2000f1e443428a5666f887be1cf75
Восков Алексей <alvoskov@gmail.com>
2024-11-14 10:44:07 +0300

    antilcg

---
+++ include/smokerand/bat_antilcg.h
@@ -0,0 +1,15 @@
+/**
+ * @file bat_antilcg.h
+ * @brief The `antilcg` battery of tests that runs in about 1 hour
+ *
+ * @copyright (c) 2024 Alexey L. Voskov, Lomonosov Moscow State University.
+ * alvoskov@gmail.com
+ *
+ * This software is licensed under the MIT license.
+ */
+#ifndef __SMOKERAND_BAT_FULL_H
+#define __SMOKERAND_BAT_FULL_H
+#include "smokerand/core.h"
+void battery_full(GeneratorInfo *gen, CallerAPI *intf,
+    unsigned int testid, unsigned int nthreads);
+#endif
